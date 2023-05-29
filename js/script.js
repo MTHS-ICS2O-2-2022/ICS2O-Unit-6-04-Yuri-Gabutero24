76cf2d9df63bbbc8162c3e80e5c2dfd3d8e38bf6
Yuri-Gabutero24 <124618894+Yuri-Gabutero24@users.noreply.github.com>
2023-05-29 17:11:21 +0000

    webpage

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
 // Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created on: May 2023
 // This file contains the JS functions for index.html
+
+"use strict"
+
+window.onLoad = function() {
+  // this calculates the volume of a sphere
+
+  const params = new URLSearchParams(window.location.search);
+
+  //input
+  
+}
